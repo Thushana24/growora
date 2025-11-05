5985540e2643656c91cf7b1056d6810e55ab6245
Thushana LS <dhushanals@example.com>
2025-11-05 11:18:47 +0530

    chore(eslint): add custom TypeScript linting rules to Next.js 16 config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,6 +5,22 @@ import nextTs from "eslint-config-next/typescript";
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+
+  //custom rules
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        { vars: "all", varsIgnorePattern: "^_", argsIgnorePattern: "^_" },
+      ],
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-empty-object-type": "off",
+      "@typescript-eslint/no-unused-expressions": "off",
+      "import/no-anonymous-default-export": "off",
+      "no-var": "off",
+    },
+  },
+  
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
