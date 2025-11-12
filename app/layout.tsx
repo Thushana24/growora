f443ad21a037c19297608467caa39a2aae0b7e81
Thushana LS <dhushanals@example.com>
2025-11-12 13:12:20 +0530

    feat: add ThemeProvider and utility for class merging with clsx & tailwind-merge

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        suppressHydrationWarning
+      suppressHydrationWarning
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <ApiProvider>
