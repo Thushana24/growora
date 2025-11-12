5fb146d91d5bcd98162feb73d1a49ea91fa0d0e6
Thushana LS <dhushanals@example.com>
2025-11-12 16:13:00 +0530

    feat(forms): add ErrorMessage component for displaying validation errors

---
+++ app/components/Forms/ErrorMessage.tsx
@@ -0,0 +1,19 @@
+import { cn } from "@/utilities/cn";
+import { ComponentProps } from "react";
+
+interface IErrorMessage extends ComponentProps<"p"> {}
+const ErrorMessage = ({ className, children }: IErrorMessage) => {
+  return (
+    <p
+      data-slot="error"
+      className={cn(
+        "min-h-6 text-sm/6 text-red-700 data-disabled:opacity-50",
+        className
+      )}
+    >
+      {children}
+    </p>
+  );
+};
+
+export default ErrorMessage;
