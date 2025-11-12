8cd4055dfb13b021f021774758692a2844ab0603
Thushana LS <dhushanals@example.com>
2025-11-12 12:55:47 +0530

    chore(layout): wrap app with ApiProvider and ReactQueryProvider
    - Integrated `ApiProvider` and `ReactQueryProvider` into `app/layout.tsx`
    - Ensures global API client and React Query state are available across the app
    - Added `suppressHydrationWarning` to handle client-side rendering consistency

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { ApiProvider } from "@/providers/ApiProvider";
+import ReactQueryProvider from "@/providers/ReactQueryProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,9 +27,14 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
+        suppressHydrationWarning
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <ApiProvider>
+         <ReactQueryProvider>
+          {children}
+         </ReactQueryProvider>
+        </ApiProvider>
       </body>
     </html>
   );
